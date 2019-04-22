3c10143cf01cea0d452591fc4878765d87ea5cac
parias <pariasm@gmail.com>
2019-04-22 16:45:59 +0200

    [script] add fun/grad cache to optimizer to prevent duplicate computations

--- scripts/optimize_prms.m
+++ scripts/optimize_prms.m
@@ -7,8 +7,22 @@ x0 = [hx1, hd1, lx1, ht1, lt1, hx2, hd2, lx2, ht2, lt2, ofw]';
 
 global computing_gradient = 0;
 
+% cache to avoid duplicate computations
+global cache_grad = zeros(size(x0));
+global cache_grad_at = zeros(size(x0));
+global cache_fun = 0;
+global cache_fun_at = zeros(size(x0));
+
 % objective function
 function mse = rbilf_train(x)
+
+	global cache_fun_at;
+	global cache_fun;
+	if norm(x - cache_fun_at) < 1e-6,
+		mse = cache_fun;
+%		fid = fopen('train20/table', 'a'); fprintf(fid, 'fun already computed \n'); fclose(fid);
+		return
+	end
 
 	sigma = 20;
 	args = sprintf(   '%.20f %.20f %.20f %.20f %.20f',       x(1), x(2), x(3), x(4),x( 5));
@@ -28,12 +42,23 @@ function mse = rbilf_train(x)
 		fid = fopen('train20/table', 'a'); fprintf(fid, [s '\n']); fclose(fid);
 	end
 
+	cache_fun = mse;
+	cache_fun_at = x;
+
 	return
 
 endfunction
 
 % gradient of objective function
 function grad = rbilf_train_grad(x)
+
+	global cache_grad;
+	global cache_grad_at;
+	if norm(x - cache_grad_at) < 1e-6,
+		grad = cache_grad;
+%		fid = fopen('train20/table', 'a'); fprintf(fid, 'grad already computed \n'); fclose(fid);
+		return
+	end
 
 	% forward difference steps
 	steps = [0.001; 0.001];
@@ -52,6 +77,9 @@ function grad = rbilf_train_grad(x)
 %		grad(i) = (rbilf_train(x + steps(:,i)) - rbilf_train(x - steps(:,i)))/2/steps(i,i);
 	end
 	computing_gradient = 0;
+
+	cache_grad = grad;
+	cache_grad_at = x;
 
 %	grad(5) = min(10,max(-10,grad(5)));
 %	disp([grad'])
