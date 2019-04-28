d192809cce259025b756785753932a6c0dc73cc9
parias <pariasm@gmail.com>
2019-04-28 13:29:30 +0200

    [script] testing different bfgs optimizer

--- scripts/optimize_prms_step1.m
+++ scripts/optimize_prms_step1.m
@@ -14,25 +14,28 @@ global cache_fun = 0;
 global cache_fun_at = zeros(size(x0));
 
 % objective function
-function mse = rbilf_train(x)
+function mse = rbilf_train(x, mode)
+
+%	if nargin == 1, verbose=0; end
+	if nargin == 1, mode='fun'; end
 
 	global cache_fun_at;
 	global cache_fun;
+
 	if norm(x - cache_fun_at) < 1e-6,
 		mse = cache_fun;
-%		fid = fopen('train20/table', 'a'); fprintf(fid, 'fun already computed \n'); fclose(fid);
-		return
+		saved = 'saved';
+	else
+		sigma = 20;
+		args = sprintf('%.20f %.20f %.20f %.20f %.20f %.20f', x(1), x(2), x(3), x(4),x(5), x(6));
+		cmd = sprintf('bin/rbilf-train-14.sh %f train20-step1 %s 2> stderr.log', sigma, args);
+		[~, mse] = system(cmd);
+		mse = str2num(mse);
+		saved = 'compu';
 	end
 
-	sigma = 20;
-	args = sprintf('%.20f %.20f %.20f %.20f %.20f %.20f', x(1), x(2), x(3), x(4),x(5), x(6));
-	cmd = sprintf('bin/rbilf-train-14.sh %f train20-step1 %s 2> stderr.log', sigma, args);
-	[~, mse] = system(cmd);
-	mse = str2num(mse);
-
-	global computing_gradient;
-	if computing_gradient == 0,
-      %            hx1   hd1   lx1   ht1   lt1   ofw   mse]';
+	if mode == 'fun',
+		%            hx1   hd1   lx1   ht1   lt1   ofw   mse]';
 		s = sprintf('%7.3f %5.3f %5.3f %7.3f %5.3f %6.3f %9.5f',...
 		             x(1),  x(2), x(3), x(4), x(5), x(6), mse);
 		disp(s);
@@ -63,12 +66,12 @@ function grad = rbilf_train_grad(x)
 
 	global computing_gradient;
 	computing_gradient = 1;
-	mse_x = rbilf_train(x);
+	mse_x = rbilf_train(x,'grd');
 
 	grad = zeros(size(x));
 	steps = diag(steps);
 	for i = 1:length(x);
-		mse_xi = rbilf_train(x + steps(:,i));
+		mse_xi = rbilf_train(x + steps(:,i),'grd');
 		grad(i) = (mse_xi - mse_x)/steps(i,i);
 %		disp([x' + steps(:,i)' , mse_xi])
 %		grad(i) = (rbilf_train(x + steps(:,i)) - rbilf_train(x - steps(:,i)))/2/steps(i,i);
@@ -84,15 +87,18 @@ function grad = rbilf_train_grad(x)
 endfunction
 
 %rbilf_train(x0)';
-rbilf_train_grad(x0)'
-
-
+%rbilf_train_grad(x0)'
 
 % bounds
 %   [hx1   , hd1, lx1,    ht1, lt1  ofw]';
 b = [0     ,   0,   0,      0,   0,   0.01]';
 B = [255.^2,   5,   1, 255.^2,   1,   5]';
 
-[x, obj, info, iter, nf, lambda] = sqp(x0, {@rbilf_train, @rbilf_train_grad},...
-                                       [], [], b, B, 100, 1e-6)
+%[x, obj, info, iter, nf, lambda] = sqp(x0, {@rbilf_train, @rbilf_train_grad},...
+%                                       [], [], b, B, 100, 1e-6)
 
+
+
+
+addpath('../lib/optim/');
+[x, histout, costdata] = projbfgs(x0, @rbilf_train, @rbilf_train_grad, B, b, 1e-6, 100)
